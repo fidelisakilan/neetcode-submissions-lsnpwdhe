7677eca34f4fdf55b28160ae0065a73e85026c54
Fidelis Akilan <fidelisakilan@proton.me>
2026-05-15 22:07:23 -0400

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == rows or c == cols or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            dfs(r+1,c)
+            dfs(r, c+1)
+            dfs(r-1, c)
+            dfs(r,c-1)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == "O" and (i in [0, rows - 1] or j in [0, cols - 1]):
+                    dfs(i, j)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+            
