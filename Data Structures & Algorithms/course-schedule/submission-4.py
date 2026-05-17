72f1b1e8d52a883be1e6f458d24d292e4ff52209
Fidelis Akilan <fidelisakilan@proton.me>
2026-05-17 12:41:21 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = defaultdict(list)
+        visitSet = set()
+        for (a,b) in prerequisites:
+            preMap[a].append(b)
+        
+        def dfs(n):
+            if n in visitSet:
+                return False
+            if preMap[n] == []:
+                return True
+
+            visitSet.add(n)
+            for child in preMap[n]:
+                if not dfs(child): return False
+            visitSet.remove(n)
+            return True
+        
+        for n in range(numCourses):
+            if not dfs(n): return False
+        return True
