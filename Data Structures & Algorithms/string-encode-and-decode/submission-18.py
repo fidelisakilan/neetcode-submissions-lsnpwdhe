d5a6eb0c010e1bdb1952c4069df8fd9b6616ead8
Fidelis Akilan <fidelisakilan@proton.me>
2026-06-07 13:45:15 -0400

    Add: string-encode-and-decode - submission-18

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-18.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        concat = ""
+        for s in strs:
+            length = len(s)
+            concat += f"{length}#{s}"
+        return concat
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        while len(s):
+            split_index = s.index("#")
+            length = int(s[:split_index])
+            res.append(s[split_index+1:split_index+length+1])
+            s = s[split_index+length+1:]
+        return res
