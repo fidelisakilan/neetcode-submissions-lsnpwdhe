060e8bf2a568e3d850519f06ed71613aca02a3ea
Fidelis Akilan <fidelisakilan@proton.me>
2026-06-08 20:35:03 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+
+        while i <= j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].upper() != s[j].upper():
+                print(i, j, s[i], s[j])
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+        
