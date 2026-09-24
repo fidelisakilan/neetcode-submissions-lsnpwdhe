305125da830767f29129380b80a9fb780fab964c
Aya <fidelisakilan@proton.me>
2026-09-24 18:19:29 -0500

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        bucket = {}
+        for i, n in enumerate(nums):
+            if target - n in bucket:
+                return [bucket[target-n], i]
+            bucket[n] = i
