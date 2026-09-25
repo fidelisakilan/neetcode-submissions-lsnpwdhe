d3a12f1e19a69e302536c76ab217331965ce1809
Aya <fidelisakilan@proton.me>
2026-09-25 08:50:13 -0500

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        bucket = [0]*26
+        for i in range(len(s)):
+            bucket[ord(s[i]) - ord('a')] += 1
+            bucket[ord(t[i]) - ord('a')] -= 1
+
+        for i in range(26):
+            if bucket[i] != 0:
+                return False
+        return True
